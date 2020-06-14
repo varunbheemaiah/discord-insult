d7908da0e4a5853740df378d53728142e2696834
varunbheemaiah <varunbheemaiah9@gmail.com>
2020-06-14 16:47:48 +0530

    logs

--- insult.py
+++ insult.py
@@ -16,10 +16,12 @@ async def on_ready():
 
 @client.event
 async def on_message(message):
+    print("IN HERE")
     if message.author == client.user:
         return
     insultToSend = random.choice(insults).strip()
     if message.content.startswith('!insult'):
+        print('SENDING')
         name = message.content.split(' ')[1]
         response = "Hey "+name+", "+insultToSend
         await message.channel.send(response)
